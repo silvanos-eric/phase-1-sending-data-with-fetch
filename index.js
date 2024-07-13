acaeeb587b1c524727468129a7a6124984e87d77
Silvanos Eric <ericoderito@gmail.com>
2024-07-13 10:09:00 +0300

    feat: extend database entities and enhance form submission
    - Add new dog entries and user profiles to db.json
    - Implement user data submission functionality in index.js
    - Clean up HTML syntax in index.html and sample_form.html

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      const idTextNode = document.createTextNode(object.id);
+      document.querySelector("body").appendChild(idTextNode);
+    })
+    .catch((error) => {
+      const { message } = error;
+      const messageTextNode = document.createTextNode(message);
+      document.querySelector("body").appendChild(messageTextNode);
+    });
+}
